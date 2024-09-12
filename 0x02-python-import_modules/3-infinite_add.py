a635edbc81e8f9ce816ef1af47a5baed499ff0d3
MauriceNgicho <mrcngicho@gmail.com>
2024-09-12 13:41:17 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total = 0 # Initialize sum to zero
+
+    # Loop through the arguments skiping thr first on(the script name)
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+
+    print(total)
