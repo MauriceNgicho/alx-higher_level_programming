9bbf1fe123b364af977c973cf3b1e29c15cc76f9
MauriceNgicho <mrcngicho@gmail.com>
2024-09-18 17:08:26 -0700

    Roman numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,8 +12,10 @@ def roman_to_int(roman_string):
     length = len(roman_string)
 
     for i in range(length):
-        # If the current character is smaller than the next one, it's a subtraction case
-        if i < length - 1 and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+        #   Subtract if current char is < next char
+        if (i < length - 1
+                and roman_dict[roman_string[i]] <
+                roman_dict[roman_string[i + 1]]):
             total -= roman_dict[roman_string[i]]
         else:
             total += roman_dict[roman_string[i]]
