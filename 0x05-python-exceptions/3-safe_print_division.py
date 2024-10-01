2c433db28ae62b78cfc7bb712ff8f5f08e2e6317
MauriceNgicho <mrcngicho@gmail.com>
2024-09-30 19:20:00 -0700

    divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        # If division by zero occurs, return None
+        result = None
+    finally:
+        # Print the result in the finally block
+        print("Inside result: {}".format(result))
+    return result  # Return the result, which may be None if there was an exception
