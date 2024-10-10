770cc7a971e896c73382dd6762851d2ff17798d1
MauriceNgicho <mrcngicho@gmail.com>
2024-10-10 12:43:57 -0700

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -19,15 +19,15 @@ def say_my_name(first_name, last_name=""):
     """
 
 
-    # check if first_name is a string
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
+# Check if first_name is provided and is a string
+    if not isinstance(first_name, str) or first_name == "":
+        raise TypeError("first_name must be a string and cannot be empty")
 
 
-    # check if last name is a string
+# check if last name is a string
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
 
-    # print full name
+# print full name
     print("My name is {} {}".format(first_name, last_name))
