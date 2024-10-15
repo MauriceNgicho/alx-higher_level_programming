f285f19b80bc14de632fb662d3458c5df24ec95a
MauriceNgicho <mrcngicho@gmail.com>
2024-10-14 19:27:57 -0700

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a function Square that inherits from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+"""imports rectangle"""
+
+
+class Square(Rectangle):
+    """Class that represents a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initializes the square with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Implements the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Overrides the str method to return square description."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
