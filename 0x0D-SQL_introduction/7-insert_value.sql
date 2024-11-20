78aec2b78f965b76e90177dcfcbe6cd88cbc1aad
MauriceNgicho <mrcngicho@gmail.com>
2024-11-19 17:39:01 -0800

    7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script to insert a new row into the table first_table in the current database
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
