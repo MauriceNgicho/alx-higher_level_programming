56d765b9cce3eb29e72e602f4ec9bc51faa16f84
MauriceNgicho <mrcngicho@gmail.com>
2024-11-19 18:07:25 -0800

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script to list all records of the table second_table
+-- Results display score and name (in this order), ordered by score (top first)
+
+SELECT score, name FROM second_table
+ORDER BY score DESC;
