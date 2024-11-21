e54f705f4db9789a5294ea7b8634449832bd3d27
MauriceNgicho <mrcngicho@gmail.com>
2024-11-20 21:08:05 -0800

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,11 @@
+-- Lists all cities along with their state names, sorted by cities.id
+SELECT 
+    cities.id, 
+    cities.name, 
+    states.name 
+FROM 
+    cities, states 
+WHERE 
+    cities.state_id = states.id 
+ORDER BY 
+    cities.id ASC;
