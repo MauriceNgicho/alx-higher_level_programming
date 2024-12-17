4effc42cdeaec1ed9b28d310ae32beee80b6a3fe
MauriceNgicho <mrcngicho@gmail.com>
2024-12-16 18:54:04 -0800

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Shebang line to specify the Node.js interpreter for Unix-like environments
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
