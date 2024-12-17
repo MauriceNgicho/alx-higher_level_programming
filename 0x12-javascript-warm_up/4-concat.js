9d3bd8f5cd94bb8fabc3b0a5f2fc7042ebf81b2c
MauriceNgicho <mrcngicho@gmail.com>
2024-12-17 12:23:10 -0800

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Shebang line to specify the Nod.js
+
+// first user argument
+const arg1 = process.argv[2];
+
+// second user argumen.
+const arg2 = process.argv[3];
+
+// Print the two args in the require format.
+console.log(`${arg1} is ${arg2}`);
