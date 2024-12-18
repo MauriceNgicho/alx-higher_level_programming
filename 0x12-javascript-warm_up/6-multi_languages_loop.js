b7c24a597060c7cc73aa72494ac7f9cb4ddca856
MauriceNgicho <mrcngicho@gmail.com>
2024-12-17 17:55:14 -0800

    multi_languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-// Shebang for Node.js
+// Shebang to specify the Node.js interpreter
+
+// Array of strings to hold the lines to print
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Loop through the array and print each line
 for (let i = 0; i < lines.length; i++) {
-	console.log(lines[i]);
+  console.log(lines[i]);
 }
