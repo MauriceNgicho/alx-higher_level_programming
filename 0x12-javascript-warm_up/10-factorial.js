0ec0de9c4ff17fb55f966d4482ccae2cc303b51b
MauriceNgicho <mrcngicho@gmail.com>
2024-12-17 18:50:06 -0800

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Shebang to specify the Node.js interpreter
+
+// Function to compute factorial recursively
+function factorial(n) {
+  if (isNaN(n) || n <= 0) {
+    return 1; // Base case: factorial of NaN or 0 is 1
+  }
+  return n * factorial(n - 1);
+}
+
+// Parse the first argument as an integer
+const num = parseInt(process.argv[2]);
+
+// Compute and print the factorial
+console.log(factorial(num));
