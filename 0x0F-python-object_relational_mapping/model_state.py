6ec9af22eb8d6b3ef6536fb93190b5b89e1e3456
MauriceNgicho <mrcngicho@gmail.com>
2024-12-30 07:33:10 -0800

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a State class and links it to the states table in a MySQL database.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+# Create the declarative base
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that links to the MySQL table `states`.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
